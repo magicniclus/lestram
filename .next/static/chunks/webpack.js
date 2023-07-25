c2cb3c2fdccbe8c00b5cf4a8425afa4b1aa6ccc6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-25 15:37:04 +0200

    update commentBanner

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cd6a8000529f8040"; }
+/******/ 		__webpack_require__.h = function() { return "66dcc324ab66793e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Comments.js
+++ components/Comments.js
@@ -44,29 +44,49 @@ const Comments = () => {
             />
           </div>
           <div className="bg-blue mt-10">
-            <div className="mx-auto max-w-7xl px-6 py-14 sm:py-24 lg:flex lg:items-center lg:justify-between lg:px-8">
-              <h2 className=" text-2xl tracking-tight text-white font-raleway">
-                Vous avez un projet d'investissement immobilier ? <br />
-                Nous sommes là pour vous accompagner.
-                <br />
-                <span className="text-or font-bold mt-3 inline-block">
-                  CONTACTEZ-NOUS !
-                </span>
-              </h2>
-              <div className="mt-10 flex flex-col gap-x-6 lg:mt-0 lg:flex-shrink-0">
-                <a
-                  href="#form"
-                  className="rounded-md text-xl px-4 py-3 font-bold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-7 bg-or max-w-[250px] font-raleway "
-                >
-                  Prendre rendez-vous !
-                </a>
-                <a
-                  href="tel:0556367038"
-                  className="text-xl leading-6 text-white flex mt-4 font-semibold font-lato"
-                >
-                  <PhoneIcon className="h-6 w-auto mr-3 text-white" />
-                  05 56 36 70 38
-                </a>
+            <div className="mx-auto max-w-7xl px-6 py-14 sm:py-24 lg:flex lg:items-center lg:justify-between lg:px-8 flex-col">
+              <div className="flex justify-between  w-full">
+                <h2 className=" text-2xl tracking-tight text-or font-raleway">
+                  Vous avez un projet d'investissement immobilier ? <br />
+                  Nous sommes là pour vous accompagner.
+                  <br />
+                  <span className="text-white font-bold mt-3 inline-block">
+                    CONTACTEZ-NOUS !
+                  </span>
+                </h2>
+                <div className="mt-10 flex flex-col gap-x-6 lg:mt-0 lg:flex-shrink-0">
+                  <a
+                    href="#form"
+                    className="rounded-md text-xl px-4 py-3 font-bold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 mt-7 bg-or max-w-[250px] font-raleway "
+                  >
+                    Prendre rendez-vous !
+                  </a>
+                  <a
+                    href="tel:0556367038"
+                    className="text-xl leading-6 text-white flex mt-4 font-semibold font-lato"
+                  >
+                    <PhoneIcon className="h-6 w-auto mr-3 text-white" />
+                    05 56 36 70 38
+                  </a>
+                </div>
+              </div>
+              <div className="mt-8 text-white text-xs">
+                <p>
+                  (1) Prix du lot 102, appartement 2 pièces de 51 m2 habitables,
+                  avec loggia et stationnement privatifs.
+                </p>
+                <p>
+                  (2) Prix du lot 104, appartement 3 pièces de 60 m2 habitables,
+                  avec loggia, terrasse et 2 stationnements privatifs.
+                </p>
+                <p>
+                  PTZ : dispositif d’aide à l’accession à la propriété
+                  permettant de bénéficier d’un prêt sans intérêt d’emprunt et
+                  sans frais de dossier, attribué sous conditions de ressources
+                  aux personnes n’ayant pas été propriétaires depuis au moins
+                  deux ans. Voir conditions détaillées auprès de nos
+                  conseillers.
+                </p>
               </div>
             </div>
           </div>
